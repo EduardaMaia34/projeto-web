5f0d32c346f1daaa3185db4efc78d3b3edd1864d
Mariana <marigsmnds@gmail.com>
2025-11-10 14:31:14 -0300

    Estante v1

---
+++ src/main/java/com/br/edu/ufersa/pw/projeto/root/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.br.edu.ufersa.pw.projeto.root.controller;
+
+import com.br.edu.ufersa.pw.projeto.livro.Model.entity.Livro;
+import com.br.edu.ufersa.pw.projeto.review.Service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getHomeInfo() {
+        List<Livro> livrosMaisRevisados = reviewService.encontrarLivrosMaisRevisadosNaSemana();
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("message", "Bem-vindo ao Bookly");
+        response.put("status", "Online");
+        response.put("version", "v1.0");
+
+        response.put("top_reviewed_livros_semana", livrosMaisRevisados);
+
+        return ResponseEntity.ok(response);
+    }
+}
